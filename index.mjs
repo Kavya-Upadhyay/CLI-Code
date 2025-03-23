14b348aa1bc9ca07904b178e6648e7d9e381317c
Kavya-Upadhyay <20kavyupadhyay@gmail.com>
2025-03-23 15:33:02 +0530

    resolved

---
+++ index.mjs
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+console.log(chalk.blue("Hello World!"));
+console.log(chalk.red.bold("Hello World!"));
+console.log(chalk.yellow.underline("Hello World!"));
+console.log(chalk.bold.underline("Harkirat"));
+
+
---
+++ kirat.js
@@ -0,0 +1,27 @@
+// const path = require("path");
+//
+// console.log(__dirname);
+// console.log(path.join(__dirname, "index.js"));
+
+// const fs = require("fs");
+// function main(filename){
+//     fs.readFile(filename, "utf-8", function(err, data){
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             let total=0;
+//             for(let i=0; i<data.length; i++){
+//                 if(data[i] === " "){
+//                     total++;
+//                 }
+//             }
+//             console.log(total+1);
+//         }
+//     })
+// }
+//
+// main(process.argv[2]);
+
+
+
